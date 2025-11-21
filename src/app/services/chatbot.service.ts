9b193c634ab1f5a569bea38566102592231bc0d7
Joaquín Emilio Huayllasco López <joaquin20040107@gmail.com>
2025-11-20 19:59:33 -0500

    Corrigiendo CORS

--- src/app/services/chatbot.service.ts
+++ src/app/services/chatbot.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError, BehaviorSubject } from 'rxjs';
 import { catchError, map, retry } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 export interface ChatMessage {
   texto: string;
